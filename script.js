24862e208eee59d5854c15608474a3e0c9d6e17b
AdorkablyNicole <hello@nicolegeorge.me>
2021-02-18 21:25:06 +1030

    loding countdown fade function added

--- script.js
+++ script.js
@@ -17,4 +17,14 @@ function blurring() {
 
 loadText.innerHTML = `${load}%`
 
+// Stack Overflow code snippet - linked below
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
+loadText.style.opacity = scale(load, 0, 100, 1, 0)
+
 }
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
